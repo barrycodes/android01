b52ef010697ccaab5a4ecbd1e4c149e8e8417dcb
barrycodes <bsmith.has.email@gmail.com>
2015-12-12 13:06:43 -0600

    minor

--- Phoneword/Phoneword/Resources/Resource.Designer.cs
+++ Phoneword/Phoneword/Resources/Resource.Designer.cs
@@ -107,6 +107,9 @@ namespace Phoneword
 			// aapt resource value: 0x7f040001
 			public const int ApplicationName = 2130968577;
 			
+			// aapt resource value: 0x7f040002
+			public const int CallHistory = 2130968578;
+			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
 			
