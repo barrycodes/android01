48668d850a32dcd7e0bdc6e36c5bd0975d9a8658
barrycodes <bsmith.has.email@gmail.com>
2015-12-12 09:39:54 -0600

    deleted button, added UI elements

--- Phoneword/Phoneword/Resources/Resource.Designer.cs
+++ Phoneword/Phoneword/Resources/Resource.Designer.cs
@@ -60,8 +60,17 @@ namespace Phoneword
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050003
+			public const int call = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int phoneword = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int translate = 2131034114;
 			
 			static Id()
 			{
